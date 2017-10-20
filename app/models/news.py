edba9945e75b34272c60e07cf939bb7346c79451
mwerumuchai <mwerumuchai@gmail.com>
2017-10-20 18:09:03 +0300

    commit changes

---
+++ app/models/news.py
@@ -0,0 +1,11 @@
+class News:
+    '''
+    News class defines news objects
+    '''
+
+    def __init__(self,id,name,description,url,category):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news_source
 
 #views
 @app.route('/')
@@ -8,8 +9,11 @@ def index():
     Function that returns the index page and its data
     '''
 
+    # getting top news highlights
+    latest_news = get_news_source("category")
+    print(latest_news)
     title = 'Home - News Highlight'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, category = latest_news)
 
 # Dynamic routing
 @app.route('/news/<int:news_id>')
