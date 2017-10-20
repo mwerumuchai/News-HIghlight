7b7c478372ae628a0c308d9e136b71c4f3391d05
mwerumuchai <mwerumuchai@gmail.com>
2017-10-20 22:17:34 +0300

    fix bug

--- app/test_news.py
+++ app/test_news.py
@@ -3,25 +3,28 @@ from models import news
 News = news.News
 
 class NewsTest(unittest.TestCase):
-    '''
-    Test class to test the behavior of the News class
-    '''
+	"""
+	Test case to test the behaviour of the news class
+	"""
 
-    def setUp(self):
-        '''
-        Set up method that runs before each TestCase
-        '''
-        self.new_news = News("bbc-sport"," BBC SPORT", "Sport outline","http://www.bbc.co.uk/sport","sport")
-    def test_instance(self):
-        '''
-        Test case that uses isinstance function
-        '''
-        self.assertTrue(isinstance(self.new_news,News))
+	def setUp(self):
+		"""
+		set up method that will run before every test
+		"""
+		self.new_news_source = News("abc-news-au","ABC News (AU)","Australia's most trusted source of local, national and world news. Comprehensive, independent, in-depth analysis, the latest business, sport, weather and more.","http://www.abc.net.au/news,"general")
 
-    # def test_init(self):
-    #     self.assertEqual(self.new_news.id,1)
-    #     self.assertEqual(self.new_news.title,'Mirror')
-        # self.assertEqual(self.new_news.image,'https://lh3.googleusercontent.com/MDO8iNBCRl_94UrF7Gfp1nY6Pb3V-u7JKsAwdLZNK5zo9qD2QdCvtFF9gjAMgx7KnA=w300')
+	def test_instance(self):
+		self.assertTrue(isinstance(self.new_news_source,News))
+
+	def test_init(self):
+		"""
+		test_init test case to test if the object is initialized porperly
+		"""
+
+		self.assertEqual(self.new_news_source.id,"abc-news-au")
+		self.assertEqual(self.new_news_source.name,"ABC News (AU)")
+		self.assertEqual(self.new_news_source.description,"Australia's most trusted source of local, national and world news. Comprehensive, independent, in-depth analysis, the latest business, sport, weather and more.")
+		self.assertEqual(self.new_news_source.url,"http://www.abc.net.au/news")
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
