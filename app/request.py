cceaa84324baffb79b4b34d100febb4f1a889b40
mwerumuchai <mwerumuchai@gmail.com>
2017-10-21 00:44:39 +0300

    add articles section

--- app/request.py
+++ app/request.py
@@ -9,6 +9,7 @@ api_key = app.config['NEWS_API_KEY']
 
 #Getting the news base url
 base_url = app.config['NEWS_API_BASE_URL']
+articles_base_url = app.config['NEWS_ARTICLES_BASE_URL']
 
 def get_news_source(category):
 	"""
@@ -52,3 +53,44 @@ def process_results(news_list):
 		news_results.append(news_source)
 
 	return news_results
+
+#Articles section
+def get_articles(source):
+	'''
+	Function that gets the json response to our url request
+	'''
+	get_articles_url = articles_base_url.format(source,api_Key)
+
+	with urllib.request.urlopen(get_articles_url,data=None) as url:
+		get_articles_data = url.read()
+		get_articles_response = json.loads(get_articles_data)
+		articles_results = None
+
+		if get_articles_response['articles']:
+			articles_results_list = get_articles_response['articles']
+			articles_results = process_articles(articles_results_list)
+
+	return articles_results
+
+def process_articles(articles_results):
+	'''
+	Function  that processes the articles result and transform them to a list of Objects
+	Args:
+	    articles_results: A list of dictionaries that contain articles details
+	Returns :
+	    articles_list: A list of articles objects
+	'''
+	articles_list = []
+	for article_item in articles_results:
+		author = article_item.get('author')
+		title = article_item.get('title')
+		description = article_item.get('description')
+		url = article_item.get('url')
+		image = article_item.get('urlToImage')
+		date = article_item.get('publishedAt')
+
+		if date:
+			article_object = Article(author,title,description,url,image,date)
+			articles_list.append(article_object)
+
+	return articles_list
